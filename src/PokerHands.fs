9cd900e314421b4ba4f6d405e7cf5b111a3aaf7f
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-13 12:47:51 +0100

    Refactor: Introduce isPair function

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -2,10 +2,13 @@ module PokerHands
 
 type HandRank = HighCard | Pair
 
+let private isPair (cards: string) =
+  cards.Split(' ')
+  |> Array.map (fun card -> card[0])
+  |> Array.groupBy id
+  |> Array.exists (fun (_, group) -> group.Length = 2)
+
 let rank (cards: string) =
-  if cards.Split(' ')
-    |> Array.map (fun card -> card[0])
-    |> Array.groupBy id
-    |> Array.exists (fun (_, group) -> group.Length = 2)
+  if isPair cards
   then Pair
   else HighCard
