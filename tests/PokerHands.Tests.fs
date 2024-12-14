36b1de335f3e4205042ef0bfaf28854a06b134f9
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-14 12:34:46 +0100

    Refactor: parameterize rank hand tests

--- tests/PokerHands.Tests.fs
+++ tests/PokerHands.Tests.fs
@@ -8,28 +8,16 @@ let assertThat actual expected = Expect.equal actual expected ""
 [<Tests>]
 let tests = testList "Poker Hands tests" [
   
-  test "Should rank a hand with High Card" {
-    let hand = "2H 3D 5S 9C KD"
-   
-    let result = rank hand
-    
-    assertThat result HighCard
-  }
-  
-  test "Should rank a hand with a Pair" {
-    let hand = "2H 2D 5S 9C KD"
-    
-    let result = rank hand
-    
-    assertThat result Pair
-  }
-  
-  test "Should rank a hand with Two Pairs" {
-    let hand = "2H 2D 5S 5C KD"
-    
-    let result = rank hand
-    
-    assertThat result TwoPairs
-  }
+  testList "Rank hand of cards scenarios" [
+    let testCases = 
+      [ ("2H 3D 5S 9C KD", HighCard)
+        ("2H 2D 5S 9C KD", Pair)
+        ("2H 2D 5S 5C KD", TwoPairs)
+        ]
+    for cards, expectedRank in testCases do
+      test $"Should rank {cards} with a {expectedRank}" {
+        assertThat (rank cards) expectedRank 
+      }
+  ]
 ]  
   
