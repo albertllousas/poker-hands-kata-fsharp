e9cd41bf8136cea1631caab9ca5be0906a5c485f
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-14 12:36:16 +0100

    Refactor: Change name of private fn

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -2,16 +2,16 @@ module PokerHands
 
 type HandRank = HighCard | Pair | TwoPairs
 
-let private countGroupsPairs (cards: string) =
+let private countGroupsOfPairs (cards: string) =
   cards.Split(' ')
   |> Array.map (fun card -> card[0])
   |> Array.groupBy id
   |> Array.filter (fun (_, group) -> group.Length = 2)
   |> Array.length
 
-let private isPair (cards: string) = countGroupsPairs cards = 1
+let private isPair (cards: string) = countGroupsOfPairs cards = 1
   
-let private isTwoPairs (cards: string) = countGroupsPairs cards = 2
+let private isTwoPairs (cards: string) = countGroupsOfPairs cards = 2
 
 let rank (cards: string) =
   if isPair cards then Pair
