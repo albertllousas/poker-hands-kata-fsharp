747e13cd9a2a2eff823c444f42242038679b9519
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-14 13:16:16 +0100

    Add case scenario: rank a hand with a Three of a kind

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -1,6 +1,6 @@
 module PokerHands
 
-type HandRank = HighCard | Pair | TwoPairs
+type HandRank = HighCard | Pair | TwoPairs | ThreeOfAKind
 
 let private countGroupsOfPairs (cards: string) =
   cards.Split(' ')
@@ -13,7 +13,14 @@ let private isPair (cards: string) = countGroupsOfPairs cards = 1
   
 let private isTwoPairs (cards: string) = countGroupsOfPairs cards = 2
 
+let private isThreeOfAKind (cards: string) =
+  cards.Split(' ')
+  |> Array.map (fun card -> card[0])
+  |> Array.groupBy id
+  |> Array.exists (fun (_, group) -> group.Length = 3)
+
 let rank (cards: string) =
   if isPair cards then Pair
   elif isTwoPairs cards then TwoPairs
+  elif isThreeOfAKind cards then ThreeOfAKind
   else HighCard
--- tests/PokerHands.Tests.fs
+++ tests/PokerHands.Tests.fs
@@ -13,6 +13,7 @@ let tests = testList "Poker Hands tests" [
       [ ("2H 3D 5S 9C KD", HighCard)
         ("2H 2D 5S 9C KD", Pair)
         ("2H 2D 5S 5C KD", TwoPairs)
+        ("2H 2D 6S 5C 2S", ThreeOfAKind)
         ]
     for cards, expectedRank in testCases do
       test $"Should rank {cards} with a {expectedRank}" {
