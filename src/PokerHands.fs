ce5a42f8f84fb26c757b9930ead86d986a065158
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-14 08:27:53 +0100

    Refactor: Reduce duplicity by introducing a function that count group of pairs

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -2,23 +2,16 @@ module PokerHands
 
 type HandRank = HighCard | Pair | TwoPairs
 
-let private isPair (cards: string) =
-  let groupsOfTwo =
-    cards.Split(' ')
-    |> Array.map (fun card -> card[0])
-    |> Array.groupBy id
-    |> Array.filter (fun (_, group) -> group.Length = 2)
-    |> Array.length
-  groupsOfTwo = 1
+let private countGroupsPairs (cards: string) =
+  cards.Split(' ')
+  |> Array.map (fun card -> card[0])
+  |> Array.groupBy id
+  |> Array.filter (fun (_, group) -> group.Length = 2)
+  |> Array.length
+
+let private isPair (cards: string) = countGroupsPairs cards = 1
   
-let private isTwoPairs (cards: string) =
-  let groupsOfTwo =
-    cards.Split(' ')
-    |> Array.map (fun card -> card[0])
-    |> Array.groupBy id
-    |> Array.filter (fun (_, group) -> group.Length = 2)
-    |> Array.length
-  groupsOfTwo = 2
+let private isTwoPairs (cards: string) = countGroupsPairs cards = 2
 
 let rank (cards: string) =
   if isPair cards then Pair
