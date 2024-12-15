ebb670a2d8a1756f4d68ebafbb3b39358e47ad82
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-15 10:14:03 +0100

    Refactor: use lists instead of string in priv fns

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -2,31 +2,31 @@ module PokerHands
 
 type HandRank = HighCard | Pair | TwoPairs | ThreeOfAKind | Straight | Flush | FullHouse | FourOfAKind | StraightFlush
 
-let private groupByValueAndCount (cards: string) =
-  cards.Split(' ')
-  |> Array.map (fun card -> card.Substring(0, card.Length - 1))
-  |> Array.groupBy id
-  |> Array.map (fun (_, group) -> group.Length)
-  |> Array.sortDescending
-  |> Array.toList
+let private groupByValueAndCount (cards: string list) =
+  cards
+  |> List.map (fun card -> card.Substring(0, card.Length - 1))
+  |> List.groupBy id
+  |> List.map (fun (_, group) -> group.Length)
+  |> List.sortDescending
   
-let private haveConsecutiveValues (cards: string) = 
+let private haveConsecutiveValues (cards: string list) = 
   let consecutiveCards aceValue =
-    cards.Split(' ')
-    |> Array.map (fun card -> card.Substring(0, card.Length - 1))
-    |> Array.map (fun card -> match card with "A" -> aceValue | "K" -> 13 | "Q" -> 12 | "J" -> 11 | _ -> int card )
-    |> Array.sort
-    |> Array.pairwise
-    |> Array.forall (fun (a, b) -> b = a + 1)
+    cards
+    |> List.map (fun card -> card.Substring(0, card.Length - 1))
+    |> List.map (fun card -> match card with "A" -> aceValue | "K" -> 13 | "Q" -> 12 | "J" -> 11 | _ -> int card )
+    |> List.sort
+    |> List.pairwise
+    |> List.forall (fun (a, b) -> b = a + 1)
   consecutiveCards 1 || consecutiveCards 14
   
-let private haveSameSuit (cards: string) =
-  cards.Split(' ')
-  |> Array.map (fun card -> card[card.Length - 1])
-  |> Array.distinct
-  |> Array.length = 1
+let private haveSameSuit (cards: string list) =
+  cards
+  |> List.map (fun card -> card[card.Length - 1])
+  |> List.distinct
+  |> List.length = 1
 
-let rank (cards: string) =
+let rank (hand: string) =
+  let cards = hand.Split(' ') |> Array.toList
   if haveSameSuit cards && haveConsecutiveValues cards then StraightFlush
   elif groupByValueAndCount cards = [4; 1] then FourOfAKind
   elif groupByValueAndCount cards = [3; 2] then FullHouse
