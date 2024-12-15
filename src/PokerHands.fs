f2b6b89f8b66f12e80bb40925f70c1dbf4fec9ff
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-15 07:32:03 +0100

    Refactor: simplify name of grouping fn

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -2,18 +2,18 @@ module PokerHands
 
 type HandRank = HighCard | Pair | TwoPairs | ThreeOfAKind
 
-let private countGroupedRanksOfSize (size: int) (cards: string) =
+let private countGroupsBySizeOf (size: int) (cards: string) =
   cards.Split(' ')
   |> Array.map (fun card -> card[0])
   |> Array.groupBy id
   |> Array.filter (fun (_, group) -> group.Length = size)
   |> Array.length
 
-let private isPair (cards: string) = countGroupedRanksOfSize 2 cards = 1
+let private isPair (cards: string) = countGroupsBySizeOf 2 cards = 1
   
-let private isTwoPairs (cards: string) = countGroupedRanksOfSize 2 cards = 2
+let private isTwoPairs (cards: string) = countGroupsBySizeOf 2 cards = 2
 
-let private isThreeOfAKind (cards: string) = countGroupedRanksOfSize 3 cards = 1
+let private isThreeOfAKind (cards: string) = countGroupsBySizeOf 3 cards = 1
 
 let rank (cards: string) =
   if isPair cards then Pair
