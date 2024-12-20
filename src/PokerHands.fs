1de0e7e70d9249954ba29d3ae8d1663c0800fa10
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-20 08:21:42 +0100

    Refactor: Introduce Card type

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -2,67 +2,78 @@ module PokerHands
 
 type HandRank = HighCard | Pair | TwoPairs | ThreeOfAKind | Straight | Flush | FullHouse | FourOfAKind | StraightFlush
 
+type Suit = H | D | S | C 
+
+type Card = int * Suit
+
 type Player = P1 | P2
 
-type Winner = Winner of player: Player * rank: HandRank * hand: string list * kicker: string option
+type Winner = Winner of player: Player * rank: HandRank * hand: Card list * kicker: int option
 
-let private groupByValueAndCount (cards: string list) =
-  cards
-  |> List.map (fun card -> card.Substring(0, card.Length - 1))
-  |> List.groupBy id
-  |> List.map (fun (_, group) -> group.Length)
-  |> List.sortDescending
-
-let private mapCardValues (aceValue: int) (hand: string list)  =
-  List.map (fun (card:string) -> card.Substring(0, card.Length - 1)) hand
-  |> List.map (fun card -> match card with "A" -> aceValue | "K" -> 13 | "Q" -> 12 | "J" -> 11 | _ -> int card )
-
-let private haveConsecutiveValues (cards: string list) = 
-  let consecutiveCards aceValue =
+module Cards =
+  
+  let groupByValueAndCount (cards: Card list) =
     cards
-    |> mapCardValues aceValue
-    |> List.sort
-    |> List.pairwise
-    |> List.forall (fun (a, b) -> b = a + 1)
-  consecutiveCards 1 || consecutiveCards 14
-  
-let private haveSameSuit (cards: string list) =
-  cards |> List.map (fun card -> card[card.Length - 1]) |> List.distinct |> List.length = 1
-  
-let private rmDupValues (hand1: string list) (hand2: string list ) =
-  let hand2Values = hand2 |> List.map (fun card -> card.Substring(0, card.Length - 1))
-  hand1
-  |> List.filter (fun card -> not (List.contains (card.Substring(0, card.Length - 1)) hand2Values ))
+    |> List.map fst
+    |> List.groupBy id
+    |> List.map (fun (_, group) -> group.Length)
+    |> List.sortDescending
 
-let valueToCard (value: int) =
-  match value with | 14 -> "A" | 1 -> "A" | 13 -> "K" | 12 -> "Q" | 11 -> "J" | _ -> value.ToString()
- 
-let private breakTie (p1Hand, p1Rank) (p2Hand, p2Rank) =
-  let highestCard (hand: string list) = hand |> mapCardValues 14 |> List.max
-  if highestCard p1Hand > highestCard p2Hand then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = None)
-  elif highestCard p1Hand < highestCard p2Hand then Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = None)
-  else
-    let p1Kicker = highestCard (rmDupValues p1Hand p2Hand)
-    let p2Kicker = highestCard (rmDupValues p2Hand p1Hand)
-    if p1Kicker > p2Kicker then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = Some (valueToCard p1Kicker))
-    else Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = Some (valueToCard p2Kicker))
+  let mapCardValues (aceValue: int) (hand: Card list)  =
+    List.map (fun (value, _) -> if value = 14 then aceValue else value) hand
 
-let rank (hand: string list) =
-  if haveSameSuit hand && haveConsecutiveValues hand then StraightFlush
-  elif groupByValueAndCount hand = [4; 1] then FourOfAKind
-  elif groupByValueAndCount hand = [3; 2] then FullHouse
-  elif haveSameSuit hand then Flush
-  elif haveConsecutiveValues hand then Straight
-  elif groupByValueAndCount hand = [3; 1; 1] then ThreeOfAKind
-  elif groupByValueAndCount hand = [2; 2; 1] then TwoPairs
-  elif groupByValueAndCount hand = [2; 1; 1; 1] then Pair
-  else HighCard
+  let haveConsecutiveValues (cards: Card list) = 
+    let consecutiveCards aceValue =
+      cards
+      |> mapCardValues aceValue
+      |> List.sort
+      |> List.pairwise
+      |> List.forall (fun (a, b) -> b = a + 1)
+    consecutiveCards 1 || consecutiveCards 14
+    
+  let haveSameSuit (cards: Card list) =
+    cards |> List.map snd |> List.distinct |> List.length = 1
+    
+  let rmDupValues (hand1: Card list) (hand2: Card list ) =
+    let hand2Values = mapCardValues 14 hand2
+    hand1
+    |> List.filter (fun (value, _) -> not (List.contains value hand2Values))
+
+module TieBreaker =
   
-let decideWinner (p1Hand: string list) (p2Hand: string list) : Winner =
-  let idx rank = List.findIndex (fun r -> r = rank) [HighCard; Pair; TwoPairs; ThreeOfAKind; Straight; Flush; FullHouse; FourOfAKind; StraightFlush]
-  let p1Rank = rank p1Hand  
-  let p2Rank = rank p2Hand
-  if idx p1Rank > idx p2Rank then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = None)
-  elif idx p1Rank < idx p2Rank then  Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = None)
-  else breakTie (p1Hand, p1Rank) (p2Hand, p2Rank)
+  let valueToCard (value: int) =
+    match value with | 14 -> "A" | 1 -> "A" | 13 -> "K" | 12 -> "Q" | 11 -> "J" | _ -> value.ToString()
+   
+  let breakTie (p1Hand, p1Rank) (p2Hand, p2Rank) =
+    let highestCard (hand: Card list) = hand |> Cards.mapCardValues 14 |> List.max
+    if highestCard p1Hand > highestCard p2Hand then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = None)
+    elif highestCard p1Hand < highestCard p2Hand then Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = None)
+    else
+      let p1Kicker = highestCard (Cards.rmDupValues p1Hand p2Hand)
+      let p2Kicker = highestCard (Cards.rmDupValues p2Hand p1Hand)
+      if p1Kicker > p2Kicker then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = Some p1Kicker)
+      else Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = Some p2Kicker)
+
+module Hands =
+  open Cards
+  open TieBreaker
+  
+  let rank (hand: Card list) =
+    if haveSameSuit hand && haveConsecutiveValues hand then StraightFlush
+    elif groupByValueAndCount hand = [4; 1] then FourOfAKind
+    elif groupByValueAndCount hand = [3; 2] then FullHouse
+    elif haveSameSuit hand then Flush
+    elif haveConsecutiveValues hand then Straight
+    elif groupByValueAndCount hand = [3; 1; 1] then ThreeOfAKind
+    elif groupByValueAndCount hand = [2; 2; 1] then TwoPairs
+    elif groupByValueAndCount hand = [2; 1; 1; 1] then Pair
+    else HighCard
+    
+  let decideWinner (p1Hand: Card list) (p2Hand: Card list) : Winner =
+    let idx rank = List.findIndex (fun r -> r = rank) [HighCard; Pair; TwoPairs; ThreeOfAKind; Straight; Flush; FullHouse; FourOfAKind; StraightFlush]
+    let p1Rank = rank p1Hand  
+    let p2Rank = rank p2Hand
+    if idx p1Rank > idx p2Rank then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = None)
+    elif idx p1Rank < idx p2Rank then  Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = None)
+    else breakTie (p1Hand, p1Rank) (p2Hand, p2Rank)
   
--- tests/PokerHands.Tests.fs
+++ tests/PokerHands.Tests.fs
@@ -10,42 +10,42 @@ let tests = testList "Poker Hands tests" [
   
   testList "Rank hand of cards scenarios" [
     let testCases = 
-      [ (["2H";"3D";"5S";"9C";"KD"], HighCard)
-        (["2H";"2D";"5S";"9C";"KD"], Pair)
-        (["2H";"2D";"5S";"5C";"KD"], TwoPairs)
-        (["2H";"2D";"6S";"5C";"2S"], ThreeOfAKind)
-        (["AD";"AS";"5C";"5S";"AH"], FullHouse)
-        (["KD";"AS";"KC";"KS";"KH"], FourOfAKind)
-        (["2D";"3D";"4D";"5D";"6H"], Straight)
-        (["7D";"8D";"9D";"10D";"JH"], Straight)
-        (["AD";"2D";"3D";"4D";"5H"], Straight)
-        (["AD";"KD";"QD";"JD";"10H"], Straight)
-        (["2D";"3D";"5D";"9D";"KD"], Flush)
-        (["2D";"2D";"5D";"9D";"KD"], Flush)
-        (["2D";"2D";"2D";"9D";"KD"], Flush)
-        (["AD";"KD";"QD";"JD";"10D"], StraightFlush)
+      [ ([(2,H);(3,D);(5,S);(9,C);(13,D)], HighCard)
+        ([(2,H);(2,D);(5,S);(9,C);(13,D)], Pair)
+        ([(2,H);(2,D);(5,S);(5,C);(13,D)], TwoPairs)
+        ([(2,H);(2,D);(6,S);(5,C);(2,S)], ThreeOfAKind)
+        ([(14,D);(14,S);(5,C);(5,S);(14,H)], FullHouse)
+        ([(13,D);(14,S);(13,C);(13,S);(13,H)], FourOfAKind)
+        ([(2,D);(3,D);(4,D);(5,D);(6,H)], Straight)
+        ([(7,D);(8,D);(9,D);(10,D);(11,H)], Straight)
+        ([(14,D);(2,D);(3,D);(4,D);(5,H)], Straight)
+        ([(14,D);(13,D);(12,D);(11,D);(10,H)], Straight)
+        ([(2,D);(3,D);(5,D);(9,D);(13,D)], Flush)
+        ([(2,D);(2,D);(5,D);(9,D);(13,D)], Flush)
+        ([(2,D);(2,D);(2,D);(9,D);(13,D)], Flush)
+        ([(14,D);(13,D);(12,D);(11,D);(10,D)], StraightFlush)
         ]
     for cards, expectedRank in testCases do
       test $"Should rank {cards} with a {expectedRank}" {
-        assertThat (rank cards) expectedRank 
+        assertThat (Hands.rank cards) expectedRank 
       }
   ]
 
   testList "Decide winner of two different hands scenarios" [
     let testCases = 
-      [ (["2H";"3D";"5S";"9C";"2D"], ["2H";"3D";"5S";"9C";"KD"], Winner(P1, Pair, ["2H";"3D";"5S";"9C";"2D"], None))
-        (["2H";"3D";"5S";"9C";"2D"], ["2H";"3D";"2S";"3C";"KD"], Winner(P2, TwoPairs, ["2H";"3D";"2S";"3C";"KD"], None))
-        (["2H";"3D";"2S";"9C";"2D"], ["2H";"3D";"2S";"3C";"KD"], Winner(P1, ThreeOfAKind, ["2H";"3D";"2S";"9C";"2D"], None))
-        (["2H";"3D";"2S";"9C";"2D"], ["AD";"2D";"3D";"4D";"5H"], Winner(P2, Straight, ["AD";"2D";"3D";"4D";"5H"], None))
-        (["AD";"AS";"5C";"4S";"AH"], ["AD";"AS";"5C";"5S";"AH"], Winner(P2, FullHouse, ["AD";"AS";"5C";"5S";"AH"], None))
-        (["KD";"AS";"KC";"KS";"KH"], ["2H";"3D";"2S";"3C";"KD"], Winner(P1, FourOfAKind, ["KD";"AS";"KC";"KS";"KH"], None))
-        (["2H";"3D";"5S";"9C";"2D"], ["AD";"KD";"QD";"JD";"10D"], Winner(P2, StraightFlush, ["AD";"KD";"QD";"JD";"10D"], None))
-        (["2H";"3D";"5S";"9C";"KD"], ["2C";"3H";"4S";"8C";"AH"], Winner(P2, HighCard, ["2C";"3H";"4S";"8C";"AH"], None))
-        (["2H";"3D";"5S";"9C";"KD"], ["2C";"3H";"4S";"8C";"KH"], Winner(P1, HighCard, ["2H";"3D";"5S";"9C";"KD"], kicker = Some "9"))
+      [ ([(2,H);(3,D);(5,S);(9,C);(2,D)], [(2,H);(3,D);(5,S);(9,C);(13,D)], Winner(P1, Pair, [(2,H);(3,D);(5,S);(9,C);(2,D)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(2,D)], [(2,H);(3,D);(2,S);(3,C);(13,D)], Winner(P2, TwoPairs, [(2,H);(3,D);(2,S);(3,C);(13,D)], None))
+        ([(2,H);(3,D);(2,S);(9,C);(2,D)], [(2,H);(3,D);(2,S);(3,C);(13,D)], Winner(P1, ThreeOfAKind, [(2,H);(3,D);(2,S);(9,C);(2,D)], None))
+        ([(2,H);(3,D);(2,S);(9,C);(2,D)], [(14,D);(2,D);(3,D);(4,D);(5,H)], Winner(P2, Straight, [(14,D);(2,D);(3,D);(4,D);(5,H)], None))
+        ([(14,D);(14,S);(5,C);(4,S);(14,H)], [(14,D);(14,S);(5,C);(5,S);(14,H)], Winner(P2, FullHouse, [(14,D);(14,S);(5,C);(5,S);(14,H)], None))
+        ([(13,D);(14,S);(13,C);(13,S);(13,H)], [(2,H);(3,D);(2,S);(3,C);(13,D)], Winner(P1, FourOfAKind, [(13,D);(14,S);(13,C);(13,S);(13,H)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(2,D)], [(14,D);(13,D);(12,D);(11,D);(10,D)], Winner(P2, StraightFlush, [(14,D);(13,D);(12,D);(11,D);(10,D)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(13,D)], [(2,C);(3,H);(4,S);(8,C);(14,H)], Winner(P2, HighCard, [(2,C);(3,H);(4,S);(8,C);(14,H)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(13,D)], [(2,C);(3,H);(4,S);(8,C);(13,H)], Winner(P1, HighCard, [(2,H);(3,D);(5,S);(9,C);(13,D)], kicker = Some 9))
         ]
     for p1Hand, p2Hand, expectedResult in testCases do
       test $"Should decide the winner two hands p1 '{p1Hand}' and p2 '{p2Hand}', with the result of {expectedResult}" {
-        assertThat (decideWinner p1Hand p2Hand) expectedResult 
+        assertThat (Hands.decideWinner p1Hand p2Hand) expectedResult 
       }
   ]
 ]  
