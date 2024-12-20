c8269c900203e0b832a02c13430e13e48485b59a
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-20 08:28:58 +0100

    Refactor: make Cards and TieBreaker as private modules

--- src/PokerHands.fs
+++ src/PokerHands.fs
@@ -10,7 +10,7 @@ type Player = P1 | P2
 
 type Winner = Winner of player: Player * rank: HandRank * hand: Card list * kicker: int option
 
-module Cards =
+module private Cards =
   
   let groupByValueAndCount (cards: Card list) =
     cards
@@ -39,12 +39,12 @@ module Cards =
     hand1
     |> List.filter (fun (value, _) -> not (List.contains value hand2Values))
 
-module TieBreaker =
+module private TieBreaker =
   
   let valueToCard (value: int) =
     match value with | 14 -> "A" | 1 -> "A" | 13 -> "K" | 12 -> "Q" | 11 -> "J" | _ -> value.ToString()
    
-  let breakTie (p1Hand, p1Rank) (p2Hand, p2Rank) =
+  let decideWinner (p1Hand, p1Rank) (p2Hand, p2Rank) =
     let highestCard (hand: Card list) = hand |> Cards.mapCardValues 14 |> List.max
     if highestCard p1Hand > highestCard p2Hand then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = None)
     elif highestCard p1Hand < highestCard p2Hand then Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = None)
@@ -55,18 +55,16 @@ module TieBreaker =
       else Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = Some p2Kicker)
 
 module Hands =
-  open Cards
-  open TieBreaker
   
   let rank (hand: Card list) =
-    if haveSameSuit hand && haveConsecutiveValues hand then StraightFlush
-    elif groupByValueAndCount hand = [4; 1] then FourOfAKind
-    elif groupByValueAndCount hand = [3; 2] then FullHouse
-    elif haveSameSuit hand then Flush
-    elif haveConsecutiveValues hand then Straight
-    elif groupByValueAndCount hand = [3; 1; 1] then ThreeOfAKind
-    elif groupByValueAndCount hand = [2; 2; 1] then TwoPairs
-    elif groupByValueAndCount hand = [2; 1; 1; 1] then Pair
+    if Cards.haveSameSuit hand && Cards.haveConsecutiveValues hand then StraightFlush
+    elif Cards.groupByValueAndCount hand = [4; 1] then FourOfAKind
+    elif Cards.groupByValueAndCount hand = [3; 2] then FullHouse
+    elif Cards.haveSameSuit hand then Flush
+    elif Cards.haveConsecutiveValues hand then Straight
+    elif Cards.groupByValueAndCount hand = [3; 1; 1] then ThreeOfAKind
+    elif Cards.groupByValueAndCount hand = [2; 2; 1] then TwoPairs
+    elif Cards.groupByValueAndCount hand = [2; 1; 1; 1] then Pair
     else HighCard
     
   let decideWinner (p1Hand: Card list) (p2Hand: Card list) : Winner =
@@ -75,5 +73,5 @@ module Hands =
     let p2Rank = rank p2Hand
     if idx p1Rank > idx p2Rank then Winner(player = P1, rank = p1Rank, hand = p1Hand, kicker = None)
     elif idx p1Rank < idx p2Rank then  Winner(player = P2, rank = p2Rank, hand = p2Hand, kicker = None)
-    else breakTie (p1Hand, p1Rank) (p2Hand, p2Rank)
+    else TieBreaker.decideWinner (p1Hand, p1Rank) (p2Hand, p2Rank)
   
--- tests/PokerHands.Tests.fs
+++ tests/PokerHands.Tests.fs
@@ -33,15 +33,33 @@ let tests = testList "Poker Hands tests" [
 
   testList "Decide winner of two different hands scenarios" [
     let testCases = 
-      [ ([(2,H);(3,D);(5,S);(9,C);(2,D)], [(2,H);(3,D);(5,S);(9,C);(13,D)], Winner(P1, Pair, [(2,H);(3,D);(5,S);(9,C);(2,D)], None))
-        ([(2,H);(3,D);(5,S);(9,C);(2,D)], [(2,H);(3,D);(2,S);(3,C);(13,D)], Winner(P2, TwoPairs, [(2,H);(3,D);(2,S);(3,C);(13,D)], None))
-        ([(2,H);(3,D);(2,S);(9,C);(2,D)], [(2,H);(3,D);(2,S);(3,C);(13,D)], Winner(P1, ThreeOfAKind, [(2,H);(3,D);(2,S);(9,C);(2,D)], None))
-        ([(2,H);(3,D);(2,S);(9,C);(2,D)], [(14,D);(2,D);(3,D);(4,D);(5,H)], Winner(P2, Straight, [(14,D);(2,D);(3,D);(4,D);(5,H)], None))
-        ([(14,D);(14,S);(5,C);(4,S);(14,H)], [(14,D);(14,S);(5,C);(5,S);(14,H)], Winner(P2, FullHouse, [(14,D);(14,S);(5,C);(5,S);(14,H)], None))
-        ([(13,D);(14,S);(13,C);(13,S);(13,H)], [(2,H);(3,D);(2,S);(3,C);(13,D)], Winner(P1, FourOfAKind, [(13,D);(14,S);(13,C);(13,S);(13,H)], None))
-        ([(2,H);(3,D);(5,S);(9,C);(2,D)], [(14,D);(13,D);(12,D);(11,D);(10,D)], Winner(P2, StraightFlush, [(14,D);(13,D);(12,D);(11,D);(10,D)], None))
-        ([(2,H);(3,D);(5,S);(9,C);(13,D)], [(2,C);(3,H);(4,S);(8,C);(14,H)], Winner(P2, HighCard, [(2,C);(3,H);(4,S);(8,C);(14,H)], None))
-        ([(2,H);(3,D);(5,S);(9,C);(13,D)], [(2,C);(3,H);(4,S);(8,C);(13,H)], Winner(P1, HighCard, [(2,H);(3,D);(5,S);(9,C);(13,D)], kicker = Some 9))
+      [ ([(2,H);(3,D);(5,S);(9,C);(2,D)],
+         [(2,H);(3,D);(5,S);(9,C);(13,D)],
+         Winner(P1, Pair, [(2,H);(3,D);(5,S);(9,C);(2,D)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(2,D)],
+         [(2,H);(3,D);(2,S);(3,C);(13,D)],
+         Winner(P2, TwoPairs, [(2,H);(3,D);(2,S);(3,C);(13,D)], None))
+        ([(2,H);(3,D);(2,S);(9,C);(2,D)],
+         [(2,H);(3,D);(2,S);(3,C);(13,D)],
+         Winner(P1, ThreeOfAKind, [(2,H);(3,D);(2,S);(9,C);(2,D)], None))
+        ([(2,H);(3,D);(2,S);(9,C);(2,D)],
+         [(14,D);(2,D);(3,D);(4,D);(5,H)],
+         Winner(P2, Straight, [(14,D);(2,D);(3,D);(4,D);(5,H)], None))
+        ([(14,D);(14,S);(5,C);(4,S);(14,H)],
+         [(14,D);(14,S);(5,C);(5,S);(14,H)],
+         Winner(P2, FullHouse, [(14,D);(14,S);(5,C);(5,S);(14,H)], None))
+        ([(13,D);(14,S);(13,C);(13,S);(13,H)],
+         [(2,H);(3,D);(2,S);(3,C);(13,D)],
+         Winner(P1, FourOfAKind, [(13,D);(14,S);(13,C);(13,S);(13,H)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(2,D)],
+         [(14,D);(13,D);(12,D);(11,D);(10,D)],
+         Winner(P2, StraightFlush, [(14,D);(13,D);(12,D);(11,D);(10,D)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(13,D)],
+         [(2,C);(3,H);(4,S);(8,C);(14,H)],
+         Winner(P2, HighCard, [(2,C);(3,H);(4,S);(8,C);(14,H)], None))
+        ([(2,H);(3,D);(5,S);(9,C);(13,D)],
+         [(2,C);(3,H);(4,S);(8,C);(13,H)],
+         Winner(P1, HighCard, [(2,H);(3,D);(5,S);(9,C);(13,D)], kicker = Some 9))
         ]
     for p1Hand, p2Hand, expectedResult in testCases do
       test $"Should decide the winner two hands p1 '{p1Hand}' and p2 '{p2Hand}', with the result of {expectedResult}" {
